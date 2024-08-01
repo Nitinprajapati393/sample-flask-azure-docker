3d4806647bc1d2965a65590d0ff4d98105ac9d1b
Nitinprajapati393 <nitinaprajapati20@gnu.ac.in>
2024-08-01 13:57:25 +0530

    code changes

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
-output "app_service_default_hostname" {
-  value = azurerm_app_service.example.default_site_hostname
-}
+# output "app_service_default_hostname" {
+#   value = azurerm_app_service.example.default_site_hostname
+# }
